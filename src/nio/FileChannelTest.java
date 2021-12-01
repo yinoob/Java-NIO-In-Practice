6da87fc6b9183d5dbc15762bae2d60b70dd73b8f
yinoob <1243876958@qq.com>
2021-12-01 21:24:35 +0800

    FileChannel实现

--- src/nio/FileChannelTest.java
+++ src/nio/FileChannelTest.java
@@ -10,19 +10,26 @@ public class FileChannelTest {
     public static void main(String[] args) throws IOException {
 
         RandomAccessFile file = new RandomAccessFile("F:\\a.txt","rw");
+
         FileChannel channel = file.getChannel();
 
+        //create buffer with capacity of 48 bytes
         ByteBuffer buffer = ByteBuffer.allocate(48);
 
+        //read into buffer
         int byteRead = channel.read(buffer);
+
         while (byteRead != -1){
 
-            System.out.println("Read" + byteRead);
+            System.out.println("Read " + byteRead);
+            //make buffer ready for read
             buffer.flip();
 
             while(buffer.hasRemaining()){
-                System.out.print((char)buffer.get());
+                // read 1 byte at a time
+                System.out.print((char)buffer.get() +" ");
             }
+            //make buffer ready for writing
             buffer.clear();
             byteRead = channel.read(buffer);
         }
